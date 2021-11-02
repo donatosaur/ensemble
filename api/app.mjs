106523fd247e96c2b92471832c23ea85ecb5f84b
fahaddd-git <66268023+fahaddd-git@users.noreply.github.com>
2021-11-02 09:08:16 -0500

    initial boilerplate for api

---
+++ api/app.mjs
@@ -0,0 +1,39 @@
+import express from 'express'
+import 'dotenv/config'
+import db from './database/db_connector.mjs'
+
+let PORT = process.env.PORT || 3306;    
+
+const app = express();    
+
+/*
+    ROUTES
+*/
+   
+app.get('/', async function(req, res)
+    {
+        // Define our queries
+        const query1 = 'DROP TABLE IF EXISTS diagnostic;';
+        const query2 = 'CREATE TABLE diagnostic(id INT PRIMARY KEY AUTO_INCREMENT, text VARCHAR(255) NOT NULL);';
+        const query3 = 'INSERT INTO diagnostic (text) VALUES ("is MySQL working?")';
+        const query4 = 'SELECT * FROM diagnostic;';
+
+        // Execute every query in an asynchronous manner, we want each query to finish before the next one starts
+       
+        try{
+            await db.query(query1)
+            await db.query(query2)
+            await db.query(query3)
+            let [results, rows]=await db.query(query4)
+            res.send(JSON.stringify(results));
+        } catch (error){
+            console.log(error)
+        }
+    });                                    
+
+/*
+    LISTENER
+*/
+app.listen(PORT, function(){            
+    console.log('Express started on http://localhost:' + PORT + '; press Ctrl-C to terminate.')
+});
---
+++ api/database/db_connector.mjs
@@ -0,0 +1,21 @@
+import mysql from 'mysql2/promise'
+import 'dotenv/config'
+
+// import * as dotenv from 'dotenv';
+// import path from 'path'
+
+// const __dirname = path.resolve()
+// dotenv.config({path: path.join(__dirname, "/.env")});
+// console.log(__dirname)
+
+
+// Create a 'connection pool' using the provided credentials
+const pool = mysql.createPool({
+    connectionLimit : 10,
+    host            : process.env.DB_HOST,
+    user            : process.env.DB_USERNAME,
+    password        : process.env.DB_PASSWORD,
+    database        : process.env.DB_NAME
+})
+
+export default pool
