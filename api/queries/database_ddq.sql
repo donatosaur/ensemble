d3351bfb4016f82a7463f3febfa1fdc641cb569a
fahaddd-git <66268023+fahaddd-git@users.noreply.github.com>
2021-11-08 12:30:00 -0600

    added sample data and tested in local instance

--- api/queries/database_ddq.sql
+++ api/queries/database_ddq.sql
@@ -127,106 +127,106 @@ COMMENT 'an intersection table that implements M:M relationships between Pieces 
 
 -- Write initial data to each table
 INSERT INTO musicians (firstName, lastName, birthdate, email, phoneNumber, street, city, state, zip, inEnsemble, active)
-VALUES (
+VALUES ("Annabelle", "Enitan", "1966-01-22", "hoyt1@gmail.com", 0222472839, "3225 Argyle Ave", "Philadelphia", "PA", 86392, 1, 0
 
        ),
-       (
+       ("Barend", "Vasu", "1982-08-14", NULL, 18293856, "300 S Delaware St", "Butler", "MO", 647730, 0, 1
 
        ),
-       (
+       ("Anja", "Lynwood", "2000-04-27", "aj.lynwood@hotmail.com", 39078965, "1412 Sunset Ave", "Chico", "CA", 95926, 0, 0
 
        );
 
-INSERT INTO instruments (instrumentID, name)
-VALUES (
+INSERT INTO instruments (name)
+VALUES ("Viola"
 
        ),
-       (
+       ("Sousaphone"
 
        ),
-       (
+       ("Glockenspiel"
 
        );
 
 
 
 INSERT INTO venues (capacity, name, street, city, state, zip)
-VALUES (
+VALUES (1742, "Orpheus Theatre", "765 S Grandview Dr", "Paoli", "IN", 47454
 
        ),
-       (
+       (22634, "Capital Theatre", "4548 Akialoa Rd", "Kekaha", "HI", 96752
 
        ),
-       (
+       (05443, "Red Rocks Theatre", "22 Harvard St", "Boston", "MA", 02124
 
        );
 
 
 INSERT INTO concert_cycles (concertTitle, startDate, endDate, conductorFirstName, conductorLastName, soloistFirstName, soloistLastName)
-VALUES (
+VALUES ("Celebrating the Season", "2021-12-20", "2021-12-24", "Alex", "Ferguson", "Carl", "Stockhausen"
 
        ),
-       (
+       ("The Music of the Spheres", "2022-05-02", "2021-05-04", "Donato", "Quartuccia", "Terrence", "Howard"
 
        ),
-       (
+       ("The Magic of Mozart", "2021-03-04", "2021-03-09", "Rebecca", "Boelzner", NULL, NULL
 
        );
 
 INSERT INTO services (startTime, endTime, isRehearsal, venueID, concertID)
-VALUES (
+VALUES ("2021-12-20 13:00:21", "2021-12-20 15:00:00", 1, 1, 1
 
        ),
-       (
+       ("2021-12-24 13:00:00", "2021-12-24 15:00:00", 0, 1, 1
 
        ),
-       (
+       ("2022-05-02 08:00:00", "2022-05-02 11:00:00", 0, NULL, 2
 
        );
 
 
-INSERT INTO pieces (pieceID, pieceTitle, composerFirstName, composerLastName, arrangerFirstName, arrangerLastName, instrumentation)
-VALUES (
+INSERT INTO pieces (pieceTitle, composerFirstName, composerLastName, arrangerFirstName, arrangerLastName, instrumentation)
+VALUES ("The Star Spanged Banner", "Francis Scott", "Key", "Leopold", "Stokowski", "2 2 2 2 - 2a 2 3 1 - T+3 - str (14.12.10.11.8)"
 
        ),
-       (
+       ("Traces", "Elizabeth", "Start", NULL, NULL, "2 2 2 2 - 2a 2 3 1 - T+3 - str (14.12.10.11.8)"
 
        ),
-       (
+       ("Concerto No. 25 for Piano and Orchestra in C Major, K. 503", "Amadeus", "Mozart", NULL, NULL, "Solo Pf - 1 2 0 2 - 2a 2 0 0 - T - str (12.10.8.6.4)"
 
        );
 
 
 INSERT INTO musicians_instruments (musicianID, instrumentID)
-VALUES (
+VALUES (1, 2
 
        ),
-       (
+       ( 2, 1
 
        ),
-       (
+       (3, 2
 
        );
 
 INSERT INTO musicians_concert_cycles (musicianID, concertID)
-VALUES (
+VALUES (2, 1
 
        ),
-       (
+       (1, 1
 
        ),
-       (
+       (1, 2
 
        );
 
 INSERT INTO pieces_concert_cycles (pieceID, concertID)
-VALUES (
+VALUES (1, 1
 
        ),
-       (
+       (2, 1
 
        ),
-       (
+       (3, 1
 
        );
 
